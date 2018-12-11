829a0e264c015e44ebc8b95a89bd3ac075c968fd
Michael Hemingway <mikemingway@gmail.com>
2018-12-11 16:17:19 -0500

    Fixed compatibility with WP 5.0 gutenberg search
    Searching was broken as the /gutenberg endpoint is automatically removed when the plugin is deactivated automatically on upgrade

--- src/features/select-post.js
+++ src/features/select-post.js
@@ -75,7 +75,7 @@ class PostSelector extends Component {
 		});
 
 		const request = apiFetch({
-			path: addQueryArgs('/gutenberg/v1/search', {
+			path: addQueryArgs('/wp/v2/search', {
 				search: value,
 				per_page: 20,
 				type: 'post',
